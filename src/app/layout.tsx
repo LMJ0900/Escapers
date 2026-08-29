68d9aa8b5e239984d430c7bc9b47fa08212e9c12
LMJ0900 <160007735+LMJ0900@users.noreply.github.com>
2026-08-29 18:53:15 +0900

    [Feature] 레이아웃에 푸터와 헤더 반영

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { fontClassName } from "@/styles/fonts";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
 
 export const metadata: Metadata = {
   title: "Escapers",
@@ -8,8 +11,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html lang="ko">
-      <body>{children}</body>
+    <html lang="ko" className={fontClassName}>
+      <body>
+        <Header>{children}</Header>
+        <Footer />
+      </body>
     </html>
   );
 }
